18b604dac1ca6a0c3869158641e6be8460158c8d
tipsun91 <drfree@qip.ru>
2016-01-06 20:53:44 +0300

    Макет постраничной навигации

--- src/ForumBundle/Controller/DefaultController.php
+++ src/ForumBundle/Controller/DefaultController.php
@@ -10,7 +10,15 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         return $this->render('@Forum/index.html.twig', [
-            'test' => 'MadCode',
+            'items' => [
+                ['name' => 'PHP',             'id' => 'cid1', 'count' => 15],
+                ['name' => 'SQL',             'id' => 'cid2', 'count' => 12],
+                ['name' => 'CSS',             'id' => 'cid3', 'count' => 11],
+                ['name' => 'JavaScript',      'id' => 'cid4', 'count' => 13],
+                ['name' => 'HTML/xHTML/WML',  'id' => 'cid5', 'count' => 14],
+                ['name' => 'Компьютеры/Софт', 'id' => 'cid6', 'count' => 17],
+                ['name' => 'Общение',         'id' => 'cid7', 'count' => 16],
+            ],
         ]);
     }
 }
