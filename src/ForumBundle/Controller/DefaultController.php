f8fa17fe66d9f4199b38f580d18dfaba2ec21e6e
tipsun91 <drfree@qip.ru>
2016-01-08 15:01:38 +0300

    Добавлены заголовки опр. форума и темы

--- src/ForumBundle/Controller/DefaultController.php
+++ src/ForumBundle/Controller/DefaultController.php
@@ -18,18 +18,22 @@ class DefaultController extends Controller
 
     public function forumAction($id)
     {
+        $forum = $this->getDoctrine()->getRepository('ForumBundle:Forum')->find($id);
         $topics = $this->getDoctrine()->getRepository('ForumBundle:Topic')->findByForum($id);
 
         return $this->render('@Forum/forum.html.twig', [
+            'forum' => $forum,
             'topics' => $topics
         ]);
     }
 
     public function topicAction($id)
     {
+        $topic = $this->getDoctrine()->getRepository('ForumBundle:Topic')->find($id);
         $posts = $this->getDoctrine()->getRepository('ForumBundle:Post')->findByTopic($id);
 
         return $this->render('@Forum/topic.html.twig', [
+            'topic' => $topic,
             'posts' => $posts
         ]);
     }
