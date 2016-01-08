612b4534ac59c7cb42ba3350585edf52ab8ca4b7
tipsun91 <drfree@qip.ru>
2016-01-08 14:37:38 +0300

    Добавлены поведения/actions, маршруты/routes, шаблоны/*.twig

--- src/ForumBundle/Controller/DefaultController.php
+++ src/ForumBundle/Controller/DefaultController.php
@@ -4,16 +4,33 @@ namespace ForumBundle\Controller;
 
 use ForumBundle\Entity\Forum;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         $forums = $this->getDoctrine()->getRepository('ForumBundle:Forum')->findAll();
 
         return $this->render('@Forum/index.html.twig', [
             'forums' => $forums
+        ]);
+    }
+
+    public function forumAction($id)
+    {
+        $topics = $this->getDoctrine()->getRepository('ForumBundle:Topic')->findByForum($id);
+
+        return $this->render('@Forum/forum.html.twig', [
+            'topics' => $topics
+        ]);
+    }
+
+    public function topicAction($id)
+    {
+        $posts = $this->getDoctrine()->getRepository('ForumBundle:Post')->findByTopic($id);
+
+        return $this->render('@Forum/topic.html.twig', [
+            'posts' => $posts
         ]);
     }
 }
