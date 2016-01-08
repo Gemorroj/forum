9b3eb2350e23a92a16e23887bd5c2c231f86d2e5
tipsun91 <drfree@qip.ru>
2016-01-08 16:21:08 +0300

    возвращен .gitkeep, добавлены тесты

--- src/ForumBundle/Controller/DefaultController.php
+++ src/ForumBundle/Controller/DefaultController.php
@@ -8,6 +8,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /**
+     * Список форумов
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
         $forums = $this->getDoctrine()->getRepository('ForumBundle:Forum')->findAll();
@@ -17,9 +22,15 @@ class DefaultController extends Controller
         ]);
     }
 
+    /**
+     * Список топиков в форуме
+     *
+     * @param Forum $forum
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function forumAction(Forum $forum)
     {
-        $topics = $this->getDoctrine()->getRepository('ForumBundle:Topic')->findByForum($forum->getId());
+        $topics = $this->getDoctrine()->getRepository('ForumBundle:Topic')->findByForum($forum);
 
         return $this->render('@Forum/forum.html.twig', [
             'forum' => $forum,
@@ -27,11 +38,19 @@ class DefaultController extends Controller
         ]);
     }
 
+    /**
+     * Сообщения в топике
+     *
+     * @param Topic $topic
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function topicAction(Topic $topic)
     {
-        $posts = $this->getDoctrine()->getRepository('ForumBundle:Post')->findByTopic($topic->getId());
+        $posts = $this->getDoctrine()->getRepository('ForumBundle:Post')->findByTopic($topic);
+        $forum = $topic->getForum();
 
         return $this->render('@Forum/topic.html.twig', [
+            'forum' => $forum,
             'topic' => $topic,
             'posts' => $posts
         ]);
--- tests/ForumBundle/Controller/DefaultControllerTest.php
+++ tests/ForumBundle/Controller/DefaultControllerTest.php
@@ -3,18 +3,45 @@
 namespace Tests\ForumBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Client;
 
 class DefaultControllerTest extends WebTestCase
 {
+    /**
+     * @var Client
+     */
+    protected static $client;
+    protected function setUp()
+    {
+        self::$client = static::createClient();
+    }
+
+
     public function testIndex()
     {
-        $client = static::createClient();
+        $crawler = self::$client->request('GET', '/');
 
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(200, self::$client->getResponse()->getStatusCode());
 
         $this->assertContains('PHP', $crawler->filter('li > a')->eq(0)->text());
         $this->assertContains('MySQL', $crawler->filter('li > a')->eq(1)->text());
+    }
+
+    public function testTopic()
+    {
+        $crawler = self::$client->request('GET', '/forum/1');
+
+        $this->assertEquals(200, self::$client->getResponse()->getStatusCode());
+
+        $this->assertContains('Test topic', $crawler->filter('li > a')->text());
+    }
+
+    public function testPost()
+    {
+        $crawler = self::$client->request('GET', '/topic/1');
+
+        $this->assertEquals(200, self::$client->getResponse()->getStatusCode());
+
+        $this->assertContains('Test post', $crawler->filter('li')->eq(1)->text());
     }
 }
