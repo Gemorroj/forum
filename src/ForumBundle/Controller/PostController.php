f8bdb4fa346f7e3b09b7098df4266c77b8d23e15
tipsun91 <drfree@qip.ru>
2016-01-10 23:31:34 +0300

    Реорганизация, начало

---
+++ src/ForumBundle/Controller/PostController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace ForumBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use ForumBundle\Entity\Topic;
+use ForumBundle\Entity\Post;
+
+/**
+ * Post controller.
+ *
+ */
+class PostController extends Controller
+{
+    /**
+     * Сообщения в топике
+     *
+     * @param Topic $topic
+     * @param int $page
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Topic $topic, $page)
+    {
+        $q = $this->getDoctrine()->getRepository('ForumBundle:Post')->getListQuery($topic);
+
+        $pager = $this->get('paginate')->paginate($q, $page);
+
+        return $this->render('@Forum/post/index.html.twig', [
+            'topic' => $topic,
+            'posts' => $pager,
+        ]);
+    }
+
+    /**
+     * Creates a new Post entity.
+     *
+     */
+    /*public function newAction(Request $request)
+    {
+        $post = new Post();
+        $form = $this->createForm('ForumBundle\Form\PostType', $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute('post_show', array('id' => $post->getId()));
+        }
+
+        return $this->render('@Forum/post/new.html.twig', array(
+            'post' => $post,
+            'form' => $form->createView(),
+        ));
+    }*/
+
+    /**
+     * Finds and displays a Post entity.
+     *
+     */
+    /*public function showAction(Post $post)
+    {
+        $deleteForm = $this->createDeleteForm($post);
+
+        return $this->render('@Forum/post/show.html.twig', array(
+            'post' => $post,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }*/
+
+    /**
+     * Displays a form to edit an existing Post entity.
+     *
+     */
+    /*public function editAction(Request $request, Post $post)
+    {
+        $deleteForm = $this->createDeleteForm($post);
+        $editForm = $this->createForm('ForumBundle\Form\PostType', $post);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute('post_edit', array('id' => $post->getId()));
+        }
+
+        return $this->render('@Forum/post/edit.html.twig', array(
+            'post' => $post,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }*/
+
+    /**
+     * Deletes a Post entity.
+     *
+     */
+    /*public function deleteAction(Request $request, Post $post)
+    {
+        $form = $this->createDeleteForm($post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($post);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('post_index');
+    }*/
+
+    /**
+     * Creates a form to delete a Post entity.
+     *
+     * @param Post $post The Post entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    /*private function createDeleteForm(Post $post)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('post_delete', array('id' => $post->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }*/
+}
