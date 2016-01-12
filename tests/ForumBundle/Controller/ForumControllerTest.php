c796b3fa8ab7bfa8027f4650eacc26cc891c3af1
Gemorroj <wapinet@gmail.com>
2016-01-12 13:12:30 +0300

    организованы тесты в соотвестсвтвии со структурой контроллеров

---
+++ tests/ForumBundle/Controller/ForumControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\ForumBundle\Controller;
+
+use Tests\ForumBundle\ForumWebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class ForumControllerTest extends ForumWebTestCase
+{
+    public function testForum()
+    {
+        $uri = self::$kernel->getContainer()->get('router')->generate('forum_show', ['id' => 1, 'page' => PHP_INT_MAX]);
+
+        $crawler = self::$client->request('GET', $uri);
+
+        $this->assertEquals(Response::HTTP_OK, self::$client->getResponse()->getStatusCode());
+
+        $this->assertContains('Test topic 1 in PHP forum', $crawler->filter('li > a')->last()->text());
+    }
+}
---
+++ tests/ForumBundle/ForumWebTestCase.php
@@ -0,0 +1,19 @@
+<?php
+namespace Tests\ForumBundle;
+
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+abstract class ForumWebTestCase extends WebTestCase
+{
+    /**
+     * @var Client
+     */
+    protected static $client;
+
+    protected function setUp()
+    {
+        self::$client = static::createClient();
+    }
+}
