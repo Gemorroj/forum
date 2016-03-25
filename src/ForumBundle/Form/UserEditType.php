96c0c317c8f6b137f79ddffc7c622b1c3a8575d2
tipsun91 <drfree@qip.ru>
2016-03-25 14:25:28 +0300

    Добавлена возможность указания пола в профиле.

--- src/ForumBundle/Form/UserEditType.php
+++ src/ForumBundle/Form/UserEditType.php
@@ -4,6 +4,7 @@ namespace ForumBundle\Form;
 
 use ForumBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -23,6 +24,14 @@ class UserEditType extends AbstractType
                 'attr' => [
                     'placeholder' => 'Пароль',
                 ]
+            ])
+            ->add('sex', ChoiceType::class, [
+                'label' => false,
+                'choices' => [
+                    'Не указывать' => null,
+                    'Мужской' => 'm',
+                    'Женский' => 'f',
+                ],
             ])
             ->add('edit', SubmitType::class, [
                 'label' => 'Готово',
