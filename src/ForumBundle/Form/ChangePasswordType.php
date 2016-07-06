3f7892642a8c3ae0cd7602e757b12c6d0f3c9e13
TiPsun91 <tipsun91@gmail.com>
2016-07-06 15:32:55 +0300

    Валидация смены пароля вынесена в файл валидации (validation.yml) для "общей картины"

--- src/ForumBundle/Form/ChangePasswordType.php
+++ src/ForumBundle/Form/ChangePasswordType.php
@@ -2,15 +2,13 @@
 
 namespace ForumBundle\Form;
 
+use ForumBundle\Model\ChangePassword;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ChangePasswordType extends AbstractType
 {
@@ -24,9 +22,6 @@ class ChangePasswordType extends AbstractType
             ->add('currentPlainPassword', PasswordType::class, [
                 'required' => true,
                 'label' => 'Действующий пароль',
-                'constraints' => new UserPassword([
-                    'message' => 'Введите действующий пароль!',
-                ]),
             ])
             ->add('plainPassword', RepeatedType::class, [
                 'required' => true,
@@ -34,15 +29,6 @@ class ChangePasswordType extends AbstractType
                 'second_options' => ['label' => 'Повторить новый пароль'],
                 'type' => PasswordType::class,
                 'invalid_message' => 'Новые пароли должны совпадать!',
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Введите новый пароль!',
-                    ]),
-                    new Length([
-                        'min' => 8,
-                        'minMessage' => 'Новый пароль должен содержать не менее {{ limit }} символа(ов)',
-                    ]),
-                ],
             ])
             ->add('save', SubmitType::class, [
                 'label' => 'Сохранить новый пароль',
@@ -56,7 +42,7 @@ class ChangePasswordType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => null,
+            'data_class' => ChangePassword::class,
             'validation_groups' => ['Default'],
         ]);
     }
